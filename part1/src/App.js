4d8e56e3e35b0912851690b786c0dd8dac3d71c0
81angelHELP98 <vangel338@gmail.com>
2022-05-31 07:12:30 -0300

    cambios

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,4 +1,5 @@
 const App = () => {
+  /*
   const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
   const exercises1 = 10
@@ -6,20 +7,31 @@ const App = () => {
   const exercises2 = 7
   const part3 = 'State of a component'
   const exercises3 = 14
+  */
+
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        excercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        excercises: 7
+      },
+      {
+        name: 'State of a component',
+        excercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header course={name} />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
   )
 }
